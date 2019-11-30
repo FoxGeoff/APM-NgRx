c3af5ade42f5935b6a66c5effa9fe18997639eb1
FoxGeoff <fox.geoffrey@gmail.com>
2019-11-29 16:24:21 -0800

    Add Login Name to reducer

--- src/app/user/state/user-reducer.ts
+++ src/app/user/state/user-reducer.ts
@@ -3,13 +3,22 @@ export function reducer(state, action) {
     switch (action.type) {
 
         case 'MASK_USER_NAME':
-            
+
             console.log('existing state: ' + JSON.stringify(state));
             console.log('payload: ' + action.payload);
 
             return {
                 ...state,
                 maskUserName: action.payload
+            };
+        case 'USER_NAME':
+
+            console.log('existing state: ' + JSON.stringify(state));
+            console.log('payload: ' + action.payload);
+
+            return {
+                ...state,
+                userName: action.payload
             };
 
         default:
