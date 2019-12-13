22ac34f123b43981de0e39fce34e169b251fce65
FoxGeoff <FoxGeoff@users.noreply.github.com>
2019-12-12 17:12:15 -0800

    Add product.actions.ts

--- src/app/products/state/product.actions.ts
+++ src/app/products/state/product.actions.ts
@@ -4,3 +4,10 @@ export enum ProductActionTypes {
     ToggleProductCode = '[Product] Toggle Product Code'
 }
 
+export class ToggleProductCode implements Action {
+    readonly type = ProductActionTypes.ToggleProductCode;
+
+    constructor (public payload: boolean) {}
+}
+
+export type ProductActions = ToggleProductCode;
